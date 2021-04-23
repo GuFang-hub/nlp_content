59c5fd737e7f8c46d4a168c95c9b37f29b1747a6
GuFang-hub <82210484+GuFang-hub@users.noreply.github.com>
2021-04-23 17:55:16 +0800

    Update TF-IDF.py

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,4 +1,4 @@
-#2021.4.23
+#2021.4.23   代码参考自https://github.com/MorvanZhou/NLP-Tutorials
 #搜索引擎(TF-IDF检索) 通过这个例子让我们熟悉nlp中重要的部分就是把语言向量化
 #我们搜索文档时会搜索关键信息，通过关键信息的检索返回结果，TF-IDF就是做信息提取的工作
 #TF：Term Frequency  词频
